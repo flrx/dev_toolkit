c1c4380a666aad5809771ef539c1b8df2bcf022c
Ibrahim Mubarak <47777720+ibrahim-mubarak@users.noreply.github.com>
2022-04-25 11:23:40 +0530

    Remove unused dependencies Fix for Flutter 2.13

--- server/lib/app.dart
+++ server/lib/app.dart
@@ -1,6 +1,3 @@
-import 'dart:io';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:macos_ui/macos_ui.dart';
 import 'package:server/connectivity.dart';
